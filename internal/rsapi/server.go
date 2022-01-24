b3bd74d411a835df0e755f3ed70e51291746f97c
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-01-24 16:15:14 -0600

    Default service port has been changed at source code
    * Now dummy endpoint to ensure everything is configured correctly
      can be verified with curl -v 127.0.0.1:10100/health-check
    
    Signed-off-by: Edwin Pĺauchu <pianodaemon@gmail.com>

--- internal/rsapi/server.go
+++ internal/rsapi/server.go
@@ -23,7 +23,7 @@ type ProbeHandler func() error
 // Configuration variables required to spin up
 // the Restful API's server
 type RestAPISettings struct {
-	Sport  int   `default:"10090"`
+	Sport  int   `default:"10100"`
 	Mmu    int64 `default:"41943040"`
 	Probes []ProbeHandler
 }
